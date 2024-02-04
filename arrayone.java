18edae17f846b1362dc291b91d0cbcd0d4382293
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-02-04 10:56:02 +0530

    arrays basics

---
+++ arrayone.java
@@ -0,0 +1,9 @@
+public class arrayone {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int numbers[] = { 8, 7, 6, 5, 4, 3, 2, 1 };
+        int maxmarks[] = { 9, 5, 6, 4, 3, 8 };
+        String fruitsname[] = { "MANGO", "ORANGE", "APPLE", "BANANA" };
+
+    }
+}
